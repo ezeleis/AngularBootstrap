3ed4031ab29ab9a88901c11b6dac6d4ad423a3a7
Facundo <facundo@MacBook-Pro-de-Mac-2.local>
2023-04-10 22:46:46 -0300

    feature(card): added a card component 'item-card' with character info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ModalComponent } from './modal/modal.component';
 import { FormComponent } from './form/form.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CardComponent } from './card/card.component';
+import { ItemCardComponent } from './item-card/item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ModalComponent,
     FormComponent,
-    NavbarComponent
+    NavbarComponent,
+    CardComponent,
+    ItemCardComponent
   ],
   imports: [
     BrowserModule,
