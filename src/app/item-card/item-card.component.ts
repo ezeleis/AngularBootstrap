ca000e11994a663d2e88b0ec1331371d47317c1c
Facundo <facundo@MacBook-Pro-de-Mac-2.local>
2023-04-10 23:29:47 -0300

    feature(item-card):dynamic card content using property binding

--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-card.component.css']
 })
 export class ItemCardComponent {
-
+  personagem: any = {
+    nome: 'Jon Snow',
+    imagem: 'https://tm.ibxk.com.br/2022/08/26/26174558850404.jpg?    ims=704x264',
+    nomeAtor: 'Kit Harington',
+    descricao:
+      'Ator ator britânico. É mais conhecido por interpretar Jon Snow na     série televisiva Game of Thrones da HBO. Meu marido, mas ele não sabe U.u',
+  };
 }
