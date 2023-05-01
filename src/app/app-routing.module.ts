32283f2ec96b0b2b151a359aa96025b513320844
ezeleispou <fleispou@gmail.com>
2023-05-01 09:34:38 -0300

    feature(login):login service save to LocalStorage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { FormComponent } from './form/form.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'card',component:CardComponent},{path:'form', component:FormComponent},{path:'login', component:LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
